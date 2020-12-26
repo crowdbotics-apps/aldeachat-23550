8e69db29a847d17eec47daceeb27b5e35cfecc13
Crowdbotics <team@crowdbotics.com>
2020-12-26 13:55:47 +0000

    Update components

--- src/features/BlankScreen0188026/screens/blank.js
+++ src/features/BlankScreen0188026/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,41 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Image
+        source={{ uri: "https://via.placeholder.com/150" }}
+        style={styles.Image_9}
+      />
+      <Text style={styles.Text_11}>Sample text content</Text>
+      <Button
+        title="Press me!"
+        style={styles.Button_13}
+        onPress={() => alert("Pressed!")}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-});
+  View_1: {},
+  Image_9: { alignSelf: "center" },
+  Text_11: { alignSelf: "center" },
+  Button_13: { width: 200, alignSelf: "center", borderRadius: 20 }
+})
